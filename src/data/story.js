4018db76cc5fde9fec8ceb802ec80e1108250e63
Toutar <132251735+Toutar@users.noreply.github.com>
2026-02-20 16:09:07 +0100

    Update story.js

--- src/data/story.js
+++ src/data/story.js
@@ -1,8 +1,8 @@
 import img1 from "../assets/images/img1.png";
 import img2 from "../assets/images/img2.png";
 import img3 from "../assets/images/img3.png";
-import img4 from "../assets/images/src/assets/images/img4.png.png";
-import img5 from "../assets/images/src/assets/images/img4.png.png";
+import img4 from "../assets/images/img4.png";
+import img5 from "../assets/images/img4.png";
 
 export const story = [
   {
