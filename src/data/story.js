71b4b9f38f394737f6badc195484f6cd0f4f19fe
Toutar <132251735+Toutar@users.noreply.github.com>
2026-02-20 15:47:14 +0100

    Update story.js

--- src/data/story.js
+++ src/data/story.js
@@ -1,8 +1,8 @@
 import img1 from "../assets/images/img1.jpg";
-import img2 from "../assets/images/img2.jpg";
-import img3 from "../assets/images/img3.jpg";
-import img4 from "../assets/images/img4.jpg";
-import img5 from "../assets/images/img5.jpg";
+import img2 from "../assets/images/img9.jpg";
+import img3 from "../assets/images/img7.jpg";
+import img4 from "../assets/images/src/assets/images/ChatGPT Image 20 févr. 2026, 15_19_12.png.jpg";
+import img5 from "../assets/images/src/assets/images/ChatGPT Image 20 févr. 2026, 15_19_12.png.jpg";
 
 export const story = [
   {
